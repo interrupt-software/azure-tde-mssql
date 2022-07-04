5020b5d64dda3716ce73400855f566e0ec20e611
gcastill0 <gilberto@hashicorp.com>
2022-07-04 12:45:38 -0400

    Updated structure for ease of writing

---
+++ explainer/js/page_01.js
@@ -0,0 +1,66 @@
+const paths = [
+    {
+        "id": 0,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "label": {
+            "text": "Setup Working Environment",
+            "class": "cell-label-top"
+        },
+        "boxtype": "cell-decorated"
+    },
+    {
+        "id": 14,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "image": "page_01_hashi-container-3-2-1.svg"
+    },
+    {
+        "id": 15,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "image": "page_01_hashi-container-3-2-2.svg"
+    },
+    {
+        "id": 16,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "image": "page_01_hashi-container-3-2-3.svg"
+    },
+    {
+        "id": 17,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "image": "page_01_hashi-container-3-2-4.svg"
+    },
+    {
+        "id": 4,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "label": {
+            "text": "Configure DEK Requirements",
+            "class": "cell-label-top"
+        },
+        "boxtype": "cell-decorated"
+    },
+    {
+        "id": 21,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "label": {
+            "text": "Encrypt an MSSQL Database",
+            "class": "cell-label-top"
+        },
+        "boxtype": "cell-decorated"
+    },
+    {
+        "id": 25,
+        "class": "cell-span-3-2",
+        "splash_delay": 3,
+        "label": {
+            "text": "Rotate Database DEK",
+            "class": "cell-label-top"
+        },
+        "boxtype": "cell-decorated"
+    }
+];
